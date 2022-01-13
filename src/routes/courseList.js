6a24ca5510c8b6afd58c6f09895ead69afcf5598
ClassBazaar <61837338+ClassBazaar@users.noreply.github.com>
2022-01-13 14:04:08 +0530

    T-0 and english courses Query updated

--- src/routes/courseList.js
+++ src/routes/courseList.js
@@ -253,6 +253,7 @@ router.get('/api/v2/courses/', async (req, res) => {
       }
 
       const dataModel = db.table('data').where((qb) => {
+        var datetime = new Date();
         if (searchQuery !== '' && filter === '') {
           qb.andWhere((subQB) => {
             subQB
@@ -261,9 +262,16 @@ router.get('/api/v2/courses/', async (req, res) => {
           });
         }
         qb.andWhere('provider', '=', p);
-        // qb.andWhere(subQB => {
-        //   subQB.where('locale', '=', `English`).orWhereRaw('locale is null');
-        // });
+        qb.andWhere(subQB => {
+          subQB.where('start_date', '>=', datetime).orWhereRaw('start_date is null');
+        });
+        qb.andWhere(subQB => {
+          subQB.where('locale', '=', `English`).orWhereRaw('locale is null');
+          
+        });
+//         qb.andWhere(subQB => {
+//           subQB.where('locale', '=', `English`).orWhereRaw('locale is null');
+//         });
 
         if (subjectFilter !== 'all') {
           // console.log('Inside the filter for subjects');
