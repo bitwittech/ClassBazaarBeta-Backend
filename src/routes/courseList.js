9e35e4ba26d454e99ef12c7c21dcbafea045327d
ClassBazaar <61837338+ClassBazaar@users.noreply.github.com>
2022-03-07 18:29:57 +0530

    test registration api edubuk

--- src/routes/courseList.js
+++ src/routes/courseList.js
@@ -877,45 +877,42 @@ router.post('/api/eduMarkUpdated', (req, res) => {
     });
 });
 
-// router.post('/api/newregistration', (req, res) => {
-//   console.log("Data Updating ",req.body)
-//   const {
-//     userid,
-//     name,
-//     gender,
-//     email_address,
-//     school_or_college_name,
-//     class_year,
-//     city,
-//     mobile_no,
-//     password
-//   } = req.body;
-
-//   db.table('newregistration')
-//     .insert({
-//       userid,
-//       name,
-//       gender,
-//       email_address,
-//       school_or_college_name,
-//       class_year,
-//       city,
-//       mobile_no,
-//       password
-//     }).onConflict('email_address')
-//     .merge()
-//     .then((data) => {
-//       res.status(200).send({
-//         status: 'User Added successfully',
-//       });
-//     })
-//     .catch((e) => {
-//       console.log('ERROR', e);
-//       res.status(500).send({
-//         status: 'Error'
-//       });
-//     });
-// });
+router.post('/api/newregistration', (req, res) => {
+  const {
+    userid,
+    name,
+    gender,
+    email_address,
+    school_or_college_name,
+    class_year,
+    city,
+    mobile_no,
+    password
+  } = req.body;
+  db.table('newregistration')
+    .insert({
+      userid,
+      name,
+      gender,
+      email_address,
+      school_or_college_name,
+      class_year,
+      city,
+      mobile_no,
+      password
+    })
+    .then((data) => {
+      res.status(200).send({
+        status: 'User Added successfully',
+      });
+    })
+    .catch((e) => {
+      console.log('ERROR', e);
+      res.status(500).send({
+        status: 'Error'
+      });
+    });
+});
 
 router.post('/api/edxresult', (req, res) => {
   const {
